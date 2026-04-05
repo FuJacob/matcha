0b610e8b5c06290758001bbcfd828155dba13653
Jacob Fu <141651335+FuJacob@users.noreply.github.com>
2026-04-04 20:04:03 -0400

    Initial Commit

---
+++ matcha-app/matcha_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  matcha_appApp.swift
+//  matcha-app
+//
+//  Created by Jacob Fu on 2026-04-04.
+//
+
+import SwiftUI
+
+@main
+struct matcha_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
